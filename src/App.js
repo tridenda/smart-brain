3b40f47c86ecebe51304a1819e5b500e0e847b75
Tri Denda <tridenda.nk@gmail.com>
2022-07-11 12:17:52 +0700

    convert to hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 
@@ -47,40 +47,31 @@ const options = {
   },
 };
 
-const initialState = {
-  input: "",
-  imageUrl: "",
-  box: {},
-  route: "signin",
-  isSignedIn: false,
-  user: {
+const App = () => {
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [box, setBox] = useState({});
+  const [route, setRoute] = useState("signin");
+  const [user, setUser] = useState({
     id: "",
     name: "",
     email: "",
     entries: 0,
     joined: "",
-  },
-};
+  });
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = initialState;
-  }
-
-  loadUser = (data) => {
-    this.setState({
-      user: {
-        id: data.id,
-        name: data.name,
-        email: data.email,
-        entries: data.entries,
-        joined: data.joined,
-      },
+  const loadUser = (data) => {
+    setUser({
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined,
     });
   };
 
-  calculateFaceLocation = (data) => {
+  const calculateFaceLocation = (data) => {
     const clarifaiFace =
       data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById("inputImage");
@@ -95,21 +86,21 @@ class App extends Component {
     };
   };
 
-  displayFaceBox = (box) => {
-    this.setState({ box: box });
+  const displayFaceBox = (box) => {
+    setBox(box);
   };
 
-  onInputChange = (event) => {
-    this.setState({ input: event.target.value });
+  const onInputChange = (event) => {
+    setInput(event.target.input);
   };
 
-  onPictureSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
+  const onPictureSubmit = () => {
+    setImageUrl(input);
     fetch("https://trd2022-smartbrain-api.herokuapp.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        input: this.state.input,
+        input,
       }),
     })
       .then((response) => response.json())
@@ -119,65 +110,70 @@ class App extends Component {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-              id: this.state.user.id,
+              id: user.id,
             }),
           })
             .then((respone) => respone.json())
             .then((count) => {
-              this.setState(Object.assign(this.state.user, { entries: count }));
+              setUser(Object.assign(user, { entries: count }));
             });
         }
-        console.log(response);
-        this.displayFaceBox(this.calculateFaceLocation(response));
+
+        displayFaceBox(calculateFaceLocation(response));
       })
       .catch((err) => console.log(err));
   };
 
-  onRouteChange = (route) => {
+  const onRouteChange = (route) => {
     if (route === "signout") {
-      this.setState(initialState);
+      setInput("");
+      setImageUrl("");
+      setBox({});
+      setRoute("");
+      setUser({
+        id: "",
+        name: "",
+        email: "",
+        entries: 0,
+        joined: "",
+      });
     } else if (route === "home") {
-      this.setState({ isSignedIn: true });
+      setIsSignedIn(true);
     }
 
-    this.setState({ route: route });
+    setRoute(route);
   };
 
-  render() {
-    const { isSignedIn, imageUrl, route, box } = this.state;
+  return (
+    <div className="App">
+      <Particles className="particles" init={particlesInit} options={options} />
+      <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
+      {route === "home" ? (
+        <>
+          <Logo />
+          <Rank name={user.name} entries={user.entries} />
+          <ImageLinkForm
+            onInputChange={onInputChange}
+            onPictureSubmit={onPictureSubmit}
+          />
+          <FaceRecognition box={box} imageUrl={imageUrl} />
+        </>
+      ) : route === "signin" ? (
+        <SignIn loadUser={loadUser} onRouteChange={onRouteChange} />
+      ) : (
+        <SignUp onRouteChange={onRouteChange} loadUser={loadUser} />
+      )}
+    </div>
+  );
+};
 
-    return (
-      <div className="App">
-        <Particles
-          className="particles"
-          init={particlesInit}
-          options={options}
-        />
-        <Navigation
-          isSignedIn={isSignedIn}
-          onRouteChange={this.onRouteChange}
-        />
-        {route === "home" ? (
-          <>
-            <Logo />
-            <Rank
-              name={this.state.user.name}
-              entries={this.state.user.entries}
-            />
-            <ImageLinkForm
-              onInputChange={this.onInputChange}
-              onPictureSubmit={this.onPictureSubmit}
-            />
-            <FaceRecognition box={box} imageUrl={imageUrl} />
-          </>
-        ) : route === "signin" ? (
-          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-        ) : (
-          <SignUp onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
-        )}
-      </div>
-    );
-  }
-}
+// class App extends Component {
+
+//   };
+
+//   render() {
+//     const { isSignedIn, imageUrl, route, box } = this.state;
+
+// }
 
 export default App;
